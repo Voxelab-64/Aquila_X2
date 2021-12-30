e0be5e3528bd8307d7f1c2ffa7b9f7b323424f4a
Voxelab-64 <73589803+Voxelab-64@users.noreply.github.com>
2021-12-30 15:36:06 +0800

    Update Configuration_adv.h

--- firmware/Sources/Marlin/Configuration_adv.h
+++ firmware/Sources/Marlin/Configuration_adv.h
@@ -134,7 +134,7 @@
   #if TEMP_SENSOR_CHAMBER
     #define CHAMBER_MINTEMP             5
     #define CHAMBER_MAXTEMP            60
-    #define TEMP_CHAMBER_HYSTERESIS     1   // (°C) Temperature proximity considered "close enough" to the target
+    #define TEMP_CHAMBER_HYSTERESIS     1   // (Â°C) Temperature proximity considered "close enough" to the target
     //#define CHAMBER_LIMIT_SWITCHING
     //#define HEATER_CHAMBER_PIN       44   // Chamber heater on/off pin
     //#define HEATER_CHAMBER_INVERTING false
@@ -295,8 +295,8 @@
     // Turn on AUTOTEMP on M104/M109 by default using proportions set here
     //#define AUTOTEMP_PROPORTIONAL
     #if ENABLED(AUTOTEMP_PROPORTIONAL)
-      #define AUTOTEMP_MIN_P      0 // (°C) Added to the target temperature
-      #define AUTOTEMP_MAX_P      5 // (°C) Added to the target temperature
+      #define AUTOTEMP_MIN_P      0 // (Â°C) Added to the target temperature
+      #define AUTOTEMP_MAX_P      5 // (Â°C) Added to the target temperature
       #define AUTOTEMP_FACTOR_P   1 // Apply this F parameter by default (overridden by M104/M109 F)
     #endif
   #endif
@@ -352,9 +352,9 @@
   //#define HOTEND_IDLE_TIMEOUT
   #if ENABLED(HOTEND_IDLE_TIMEOUT)
     #define HOTEND_IDLE_TIMEOUT_SEC (5*60)    // (seconds) Time without extruder movement to trigger protection
-    #define HOTEND_IDLE_MIN_TRIGGER   180     // (°C) Minimum temperature to enable hotend protection
-    #define HOTEND_IDLE_NOZZLE_TARGET   0     // (°C) Safe temperature for the nozzle after timeout
-    #define HOTEND_IDLE_BED_TARGET      0     // (°C) Safe temperature for the bed after timeout
+    #define HOTEND_IDLE_MIN_TRIGGER   180     // (Â°C) Minimum temperature to enable hotend protection
+    #define HOTEND_IDLE_NOZZLE_TARGET   0     // (Â°C) Safe temperature for the nozzle after timeout
+    #define HOTEND_IDLE_BED_TARGET      0     // (Â°C) Safe temperature for the bed after timeout
   #endif
 
   // @section temperature
@@ -826,7 +826,7 @@
   //#define HOME_AFTER_DEACTIVATE  // Require rehoming after steppers are deactivated
 
   // Minimum time that a segment needs to take if the buffer is emptied
-  #define DEFAULT_MINSEGMENTTIME        20000   // (µs)
+  #define DEFAULT_MINSEGMENTTIME        20000   // (Âµs)
 
   // Slow down the machine if the look ahead buffer is (by default) half full.
   // Increase the slowdown divisor for larger buffer sizes.
@@ -895,7 +895,7 @@
    * and hotend offsets.
    *
    * Note: HOTEND_OFFSET and CALIBRATION_OBJECT_CENTER must be set to within
-   *       ±5mm of true values for G425 to succeed.
+   *       Â±5mm of true values for G425 to succeed.
    */
   //#define CALIBRATION_GCODE
   #if ENABLED(CALIBRATION_GCODE)
@@ -1358,7 +1358,7 @@
      */
     #if ENABLED(U8GLIB_ST7920)
       // Enable this option and reduce the value to optimize screen updates.
-      // The normal delay is 10µs. Use the lowest value that still gives a reliable display.
+      // The normal delay is 10Âµs. Use the lowest value that still gives a reliable display.
       //#define DOGM_SPI_DELAY_US 5
 
       //#define LIGHTWEIGHT_UI
@@ -1384,7 +1384,7 @@
     //#define STATUS_FAN_FRAMES 3       // :[0,1,2,3,4] Number of fan animation frames
     //#define STATUS_HEAT_PERCENT       // Show heating in a progress bar
     //#define BOOT_MARLIN_LOGO_SMALL    // Show a smaller Marlin logo on the Boot Screen (saving 399 bytes of flash)
-    //#define BOOT_MARLIN_LOGO_ANIMATED // Animated Marlin logo. Costs ~�?260 (or ~940) bytes of PROGMEM.
+    //#define BOOT_MARLIN_LOGO_ANIMATED // Animated Marlin logo. Costs ~â?260 (or ~940) bytes of PROGMEM.
 
     // Frivolous Game Options
     //#define MARLIN_BRICKOUT
@@ -1487,16 +1487,16 @@
       #if ENABLED(TOUCH_UI_UTF8_WESTERN_CHARSET)
         // Additional character groups. These characters require
         // full bitmaps and take up considerable storage:
-        //#define TOUCH_UI_UTF8_SUPERSCRIPTS  // ¹ ² ³
-        //#define TOUCH_UI_UTF8_COPYRIGHT     // © ®
-        //#define TOUCH_UI_UTF8_GERMANIC      // ß
-        //#define TOUCH_UI_UTF8_SCANDINAVIAN  // Æ Ð Ø Þ æ ð ø þ
-        //#define TOUCH_UI_UTF8_PUNCTUATION   // « » ¿ ¡
-        //#define TOUCH_UI_UTF8_CURRENCY      // ¢ £ ¤ ¥
-        //#define TOUCH_UI_UTF8_ORDINALS      // º ª
-        //#define TOUCH_UI_UTF8_MATHEMATICS   // ± × ÷
-        //#define TOUCH_UI_UTF8_FRACTIONS     // ¼ ½ ¾
-        //#define TOUCH_UI_UTF8_SYMBOLS       // µ ¶ ¦ § ¬
+        //#define TOUCH_UI_UTF8_SUPERSCRIPTS  // Â¹ Â² Â³
+        //#define TOUCH_UI_UTF8_COPYRIGHT     // Â© Â®
+        //#define TOUCH_UI_UTF8_GERMANIC      // Ã
+        //#define TOUCH_UI_UTF8_SCANDINAVIAN  // Ã Ã Ã Ã Ã¦ Ã° Ã¸ Ã¾
+        //#define TOUCH_UI_UTF8_PUNCTUATION   // Â« Â» Â¿ Â¡
+        //#define TOUCH_UI_UTF8_CURRENCY      // Â¢ Â£ Â¤ Â¥
+        //#define TOUCH_UI_UTF8_ORDINALS      // Âº Âª
+        //#define TOUCH_UI_UTF8_MATHEMATICS   // Â± Ã Ã·
+        //#define TOUCH_UI_UTF8_FRACTIONS     // Â¼ Â½ Â¾
+        //#define TOUCH_UI_UTF8_SYMBOLS       // Âµ Â¶ Â¦ Â§ Â¬
       #endif
     #endif
 
@@ -1795,7 +1795,7 @@
 
   /**
    * Minimum delay before and after setting the stepper DIR (in ns)
-   *     0 : No delay (Expect at least 10µS since one Stepper ISR must transpire)
+   *     0 : No delay (Expect at least 10ÂµS since one Stepper ISR must transpire)
    *    20 : Minimum for TMC2xxx drivers
    *   200 : Minimum for A4988 drivers
    *   400 : Minimum for A5984 drivers
@@ -1810,7 +1810,7 @@
   //#define MINIMUM_STEPPER_PRE_DIR_DELAY 650
 
   /**
-   * Minimum stepper driver pulse width (in µs)
+   * Minimum stepper driver pulse width (in Âµs)
    *   0 : Smallest possible width the MCU can produce, compatible with TMC2xxx drivers
    *   0 : Minimum 500ns for LV8729, adjusted in stepper.h
    *   1 : Minimum for A4988 and A5984 stepper drivers
@@ -1911,17 +1911,17 @@
   // Therefore some clients abort after 30 seconds in a timeout.
   // Some other clients start sending commands while receiving a 'wait'.
   // This "wait" is only sent when the buffer is empty. 1 second is a good value here.
-  //#define NO_TIMEOUTS 1000 // Milliseconds
+  #define NO_TIMEOUTS 200 // Milliseconds
 
   // Some clients will have this feature soon. This could make the NO_TIMEOUTS unnecessary.
-  //#define ADVANCED_OK
+  #define ADVANCED_OK
 
   // Printrun may have trouble receiving long strings all at once.
   // This option inserts short delays between lines of serial output.
   #define SERIAL_OVERRUN_PROTECTION
 
   // For serial echo, the number of digits after the decimal point
-  //#define SERIAL_FLOAT_PRECISION 4
+  #define SERIAL_FLOAT_PRECISION 4
 
   // @section extras
 
@@ -2833,9 +2833,9 @@
      *  Example pulse data for Nikon: https://bit.ly/2FKD0Aq
      *                     IR Wiring: https://git.io/JvJf7
      */
-    //#define PHOTO_PULSES_US { 2000, 27850, 400, 1580, 400, 3580, 400 }  // (µs) Durations for each 48.4kHz oscillation
+    //#define PHOTO_PULSES_US { 2000, 27850, 400, 1580, 400, 3580, 400 }  // (Âµs) Durations for each 48.4kHz oscillation
     #ifdef PHOTO_PULSES_US
-      #define PHOTO_PULSE_DELAY_US 13 // (µs) Approximate duration of each HIGH and LOW pulse in the oscillation
+      #define PHOTO_PULSE_DELAY_US 13 // (Âµs) Approximate duration of each HIGH and LOW pulse in the oscillation
     #endif
   #endif
 
@@ -3348,7 +3348,7 @@
     //#define MAX7219_GCODE          // Add the M7219 G-code to control the LED matrix
     #define MAX7219_INIT_TEST    2   // Test pattern at startup: 0=none, 1=sweep, 2=spiral
     #define MAX7219_NUMBER_UNITS 1   // Number of Max7219 units in chain.
-    #define MAX7219_ROTATE       0   // Rotate the display clockwise (in multiples of +/- 90°)
+    #define MAX7219_ROTATE       0   // Rotate the display clockwise (in multiples of +/- 90Â°)
                                     // connector at:  right=0   bottom=-90  top=90  left=180
     //#define MAX7219_REVERSE_ORDER  // The individual LED matrix units may be in reversed order
     //#define MAX7219_SIDE_BY_SIDE   // Big chip+matrix boards can be chained side-by-side
